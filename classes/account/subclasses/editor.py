77f99fabc858833ed91c189535438c6c78c041f8
Sanya Baranov <gettofarmila@gmail.com>
2026-05-12 22:16:30 +0300

    added: toggle on/off lots

--- classes/account/subclasses/editor.py
+++ classes/account/subclasses/editor.py
@@ -7,7 +7,11 @@ class FunPayEditor:
 
     async def change_lot_price(self, lot_id, new_price: str):
         '''
-        Func change lot price
+        Func change lot price, accept:
+            lot_id
+            new_price: str
+        Returns True if change, if not change raise LotEditingError, 
+        raise RequestError if bad internet 
         '''
         lot = await self.account.lot.get_lot_editor_details(lot_id)
         lot.fields['price'] = new_price
@@ -17,4 +21,23 @@ class FunPayEditor:
             if new_lot.fields['price'] == lot.fields['price']:
                 return True
             raise LotEditingError('Changing lot price error')
+        raise RequestError()
+
+    async def toggle_off_lot(self, lot_id):
+        '''
+
+        '''
+        lot = await self.account.lot.get_lot_editor_details(lot_id)
+        response = await self.account.client.edit_lot(lot)
+        if response.status_code == 200:
+            return True
+        raise RequestError()
+
+    async def toggle_on_lot(self, lot_id):
+        '''
+        '''
+        lot = await self.account.lot.get_lot_editor_details(lot_id)
+        response = await self.account.client.edit_lot(lot, active=True)
+        if response.status_code == 200:
+            return True
         raise RequestError()
